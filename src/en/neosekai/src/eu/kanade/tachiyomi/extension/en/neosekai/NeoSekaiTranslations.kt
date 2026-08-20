e38e090e2a237bfc2ec027004d1eb7a43183e1de
oussama <mrissaoussama@gmail.com>
2026-08-20 18:09:59 +0100

    Remove dead/duplicate extensions
    - allnovel: allnovel.org now just 302-redirects to novelfull.com,
      already covered by the existing NovelFull extension.
    - lightnovelworld, lightnovelplus, neosekai (NeoSekai Translations),
      srankmanga, systemtranslation, panchotranslations: removed per
      request.
    
    lightnovelpub.com and novelsonline.org were also requested for
    removal but don't exist under any name in this fork - nothing to do.

--- src/en/neosekai/src/eu/kanade/tachiyomi/extension/en/neosekai/NeoSekaiTranslations.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.novelextension.en.neosekai
-
-import eu.kanade.tachiyomi.multisrc.madaranovel.MadaraNovel
-import keiyoushi.annotation.Source
-
-@Source
-abstract class NeoSekaiTranslations : MadaraNovel()
--- src/es/panchotranslations/src/eu/kanade/tachiyomi/extension/es/panchotranslations/PanchoTranslations.kt
+++
@@ -1,9 +0,0 @@
-﻿package eu.kanade.tachiyomi.novelextension.es.panchotranslations
-
-import eu.kanade.tachiyomi.multisrc.madaranovel.MadaraNovel
-import keiyoushi.annotation.Source
-
-@Source
-abstract class PanchoTranslations : MadaraNovel() {
-    override val useNewChapterEndpointDefault = true
-}
