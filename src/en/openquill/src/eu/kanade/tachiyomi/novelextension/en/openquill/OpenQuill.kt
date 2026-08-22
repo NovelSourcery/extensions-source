9817aa75b441b39d4138b8f062f345ca49d8e4d0
oussama <mrissaoussama@gmail.com>
2026-08-22 16:35:21 +0100

    fix(openquill): correct package to eu.kanade.tachiyomi.novelextension.en.openquill

--- src/en/openquill/src/eu/kanade/tachiyomi/novelextension/en/openquill/OpenQuill.kt
+++ src/en/openquill/src/eu/kanade/tachiyomi/novelextension/en/openquill/OpenQuill.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.extension.en.openquill
+package eu.kanade.tachiyomi.novelextension.en.openquill
 
 import eu.kanade.tachiyomi.source.NovelSource
 import eu.kanade.tachiyomi.source.model.Filter
